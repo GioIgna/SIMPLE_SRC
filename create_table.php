50999b45fd5c8425c49ae1d1f4e62561c89afe57
GioIgna <ignazio.santacaterina.79@gmail.com>
2016-11-10 09:49:56 +0100

    Update create_table.php
    Verifica l'esistenza della tabella

--- create_table.php
+++ create_table.php
@@ -1,22 +1,30 @@
 <?php
 require 'conn_db.php';
-
-$query_table="
-		CREATE TABLE IF NOT EXISTS Utenti
-			(
-			ID_User INT(15) NOT NULL AUTO_INCREMENT,
-				Username VARCHAR(255) NOT NULL,
-				Password VARCHAR(255) NOT NULL,
-				Email VARCHAR(255) NOT NULL,
-				reg_date TIMESTAMP,
-			PRIMARY KEY (ID_User)
-			)";
-if(mysqli_query($db_conn, $query_table)){
-	echo "TABELLA CREATA CON SUCCESSO<br>";
-}	else {
-	echo "ERRORE:".mysqli_error($db_server). "<br>";
+//verifica l'esistenza della tabella
+$query_verify="SELECT * FROM Utenti";
+if(mysqli_query($db_conn, $query_verify)){
+	
+	echo "TABELLA GIA' ESISTENTE NEL DB";
+	
+} else{
+		
+	//query creazione tabella
+	$query_table="
+			CREATE TABLE IF NOT EXISTS Utenti
+				(
+				ID_User INT(15) NOT NULL AUTO_INCREMENT,
+					Username VARCHAR(255) NOT NULL,
+					Password VARCHAR(255) NOT NULL,
+					Email VARCHAR(255) NOT NULL,
+					reg_date TIMESTAMP,
+				PRIMARY KEY (ID_User)
+				)";
+	if(mysqli_query($db_conn, $query_table)){
+		echo "TABELLA CREATA CON SUCCESSO<br>";
+	}	else {
+		echo "ERRORE:".mysqli_error($db_server). "<br>";
 }
-
+}
 mysqli_close($db_server);
 
 ?>
